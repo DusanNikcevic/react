ad628718d5c5e5abbfbf92c9e0cb7b934da9e41e
pho7on <nikdu4@gmail.com>
2019-06-16 13:40:39 +0200

    react-boilerplate0.1.0

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <section id="App">
+      <div className="test">
+        <h1>React Boilerplate</h1>
+      </div>
+    </section>
+  );
+};
+
+export default App;
